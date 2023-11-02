f9eeec7d6893eddb94e203a31c8cb56364d6fcc0
Josh <95011683+jopo86@users.noreply.github.com>
2023-11-02 17:31:37 -0400

    Delete QuizMaker directory

--- QuizMaker/src/WrittenQuestion.cpp
+++
@@ -1,6 +0,0 @@
-#include "WrittenQuestion.h"
-
-WrittenQuestion::WrittenQuestion(std::string question, std::string answer) {
-	this->question = question;
-	this->answer = answer;
-}
--- QuizMaker/src/WrittenQuestion.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Question.h"
-
-class WrittenQuestion : public Question
-{
-public:
-	WrittenQuestion(std::string question, std::string answer);
-};
